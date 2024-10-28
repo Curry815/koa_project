a8e2aa080744c3860fef506d4a2988f290feffcb
curry_815 <1274750944@qq.com>
2024-10-28 23:28:37 +0800

    feat: 验证登录

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -1,18 +1,23 @@
-const Router = require('koa-router');
+const Router = require("koa-router");
 
 // 引入中间件
-const { userValidator, verifyUser, crpytPassword } = require('../middleware/user.middleware')
-const { register, login } = require('../controller/user.controller');
+const {
+  userValidator,
+  verifyUser,
+  crpytPassword,
+  verifyLogin,
+} = require("../middleware/user.middleware");
+const { register, login } = require("../controller/user.controller");
 
 const router = new Router({
   // 路由前缀
-  prefix: '/users'
+  prefix: "/users",
 });
 
 // 注册接口
-router.post('/register', userValidator, verifyUser, crpytPassword, register);
+router.post("/register", userValidator, verifyUser, crpytPassword, register);
 
 // 登录接口
-router.post('/login', login);
+router.post("/login", userValidator, verifyLogin, login);
 
 module.exports = router;
