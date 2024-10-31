9f738c4a4f42ed62eef02c25d8a8b8a9f72321b8
curry_815 <1274750944@qq.com>
2024-10-31 17:40:13 +0800

    feat：更新地址接口

--- src/router/addr.route.js
+++ src/router/addr.route.js
@@ -3,7 +3,7 @@ const Router = require("koa-router");
 const { auth } = require("../middleware/auth.middleware");
 const { validator } = require("../middleware/addr.middleware");
 
-const { create, findAll } = require("../controller/addr.controller");
+const { create, findAll, update } = require("../controller/addr.controller");
 
 const router = new Router({
   prefix: "/address",
@@ -23,5 +23,17 @@ router.post(
 
 // 获取地址列表接口
 router.get("/", auth, findAll);
+
+// 更新地址接口
+router.put(
+  "/:id",
+  auth,
+  validator({
+    consignee: "string",
+    phone: { type: "string", format: /^1\d{10}$/ },
+    address: "string",
+  }),
+  update
+);
 
 module.exports = router;
